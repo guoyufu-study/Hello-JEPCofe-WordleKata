0005b6d299cb96638c087176e48f8cf22a8a7977
guoyufu-study <guoyufu_study@163.com>
2022-04-05 01:52:04 +0800

    基本配置及测试用例

---
+++ src/main/java/cn/jasper/java/features/wordle/Wordle.java
@@ -0,0 +1,8 @@
+package cn.jasper.java.features.wordle;
+
+public record Wordle(String hidden) {
+
+    public String guess(String guess) {
+        return guess;
+    }
+}
---
+++ src/test/java/cn/jasper/java/features/wordle/WordleTest.java
@@ -0,0 +1,14 @@
+package cn.jasper.java.features.wordle;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class WordleTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/wordle.csv"})
+    void wordle(String hidden, String guess, String expected) {
+        Assertions.assertThat(new Wordle(hidden).guess(guess)).isEqualTo(expected);
+    }
+}
